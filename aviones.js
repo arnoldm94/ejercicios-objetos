2b8d02bd6dc3786d6d743dff7315d8f92d304195
Arnold Melet <arnoldmelet18@gmail.com>
2024-05-17 20:47:55 +0200

    falta terminar ultimo ejercicio

--- aviones.js
+++ aviones.js
@@ -1,6 +1,45 @@
-/* Existen 2 clases y varios "arrays": Gallery, Painter y 4 "arrays" con rutas a archivos de imágenes, divididos por tipo de vehículo y uso.
+// Existen 2 clases y varios "arrays": Gallery, Painter y 4 "arrays" con rutas a archivos de imágenes,
+// divididos por tipo de vehículo y uso.
+class Gallery {
+constructor(conj1, conj2) {
+this.conj1 =conj1
+this.conj2 = conj2
+getRandomCivil = () => {
+console.log('pongo imagen CIVIL aleatoria')
+}
+getRandomMilitary = () => {
+console.log('pongo imagen MILITAR aleatoria')
+}
+getAll = () => {
+console.log('Muestro todas las imagenes')
+}
 
-Gallery: Su única funcionalidad es albergar imágenes y devolverlas dependiendo del método que utilicemos. No interactúa con HTML.
+}}
+class Painter {
+    constructor(gallery) {
+    this.gallery = gallery
+    createGallery = () => {
+    console.log('Creo section y agrego al body')
+}
+    createImageTag = () =>{ 
+    console.log('Acepta la url de una imagen y devuelve los siguientes elementos:') /*
+    <picture>
+        <img src="ThisIsAnImage.jpg" />
+    </picture>*/
+        }
+        paintSingleImage = () => {
+            console.log('Acepta la url de una imagen y agrega a gallery el elemento creado por createImageTag.')
+        }
+        paintMultipleImages = () => {
+            console.log('Acepta un conjunto de imágenes y agrega a gallery, uno a uno, el elemento creado por createImageTag.')
+        }
+    
+    }}
+
+    
+/*
+Gallery: Su única funcionalidad es albergar imágenes y devolverlas dependiendo del método que utilicemos. 
+No interactúa con HTML.
 
 constructor: Acepta dos parámetros, ambos conjuntos de imágenes.
 
@@ -10,7 +49,8 @@ getRandomMilitary: devuelve un vehículo aleatorio militar de la galería.
 
 getAll: devuelve el conjunto de vehículos de la galería, tanto militares como civiles.
 
-Painter: La clase encargada de pintar las imágenes, de interactuar con el DOM. Encargada de crear etiquetas y manipular el DOM para agregarlas.
+Painter: La clase encargada de pintar las imágenes, de interactuar con el DOM. Encargada de crear etiquetas
+ y manipular el DOM para agregarlas.
 
 constructor: Ejecutará la función createGallery.
 
